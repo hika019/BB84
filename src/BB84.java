20f5733b086e462d0164e988830369d1b89d03c8
hika019 <ikazuti2001@gmail.com>
2022-05-31 03:19:02 +0900

    Aliceによるqbitの選定と表示

--- src/BB84.java
+++ src/BB84.java
@@ -1,16 +1,32 @@
+import java.util.Random;
 
 public class BB84 {
 
 	public static void main(String[] args) {
-		// TODO 自動生成されたメソッド・スタブ
-		Ket a = new Ket(0);
-		Ket b = new Ket(1);
+		Random rand = new Random();
 		
-		Ket c = new Ket();
-		c.TensorProduct(a, b);
-		a.Show();
-		b.Show();
-		c.Show();
+		BraKetVector[] Alice_qbit = new BraKetVector[4];
+		int[] Alice_bit = new int[Alice_qbit.length];
+		
+		for(int i=0; i<Alice_qbit.length; i++) {
+			int tmp = rand.nextInt(16)%4;
+			Alice_qbit[i] = new BraKetVector(tmp); 
+			Alice_bit[i] = tmp%2;
+		}
+		
+		
+		//Show Alice data
+		for(int i=0; i<Alice_qbit.length; i++) {
+			Alice_qbit[i].Show();
+			System.out.println(Alice_bit[i]);
+		}
+		
+		
+		
+		
+		
+		
+		
 	}
 
 }
