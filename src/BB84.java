a3e2878ec17d1d897b69c31dd933132344fbf6bd
hika019 <ikazuti2001@gmail.com>
2022-05-30 15:22:23 +0900

    ケットベクトル0, 1の追加とテンソル積の追加

--- src/BB84.java
+++ src/BB84.java
@@ -3,7 +3,14 @@ public class BB84 {
 
 	public static void main(String[] args) {
 		// TODO 自動生成されたメソッド・スタブ
-
+		Ket a = new Ket(0);
+		Ket b = new Ket(1);
+		
+		Ket c = new Ket();
+		c.TensorProduct(a, b);
+		a.Show();
+		b.Show();
+		c.Show();
 	}
 
 }
