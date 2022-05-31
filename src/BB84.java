cfc6c6dac6551fc88874c02b0cecfaf7d70eacd4
hika019 <ikazuti2001@gmail.com>
2022-05-31 16:06:46 +0900

    盗聴機能の追加

--- src/BB84.java
+++ src/BB84.java
@@ -2,36 +2,36 @@ import java.util.Random;
 
 public class BB84 {
 
+	/*
+	 * measurement: 01系, +-系測定のパラメータ 0:01, 1:+-
+	 */
+	
 	public static void main(String[] args) {
 		Random rand = new Random();
 		
 		int bitLength = 4;
 		
 		
+		
+		
 		//Alice
-		int tmp = rand.nextInt(16)%4;
-		BraKetVector Alice_qbit = new BraKetVector(tmp); 
-		int Alice_bit = tmp%2;
-		int Alice_measurement = 0;
-		if(tmp < 2) Alice_measurement = 0;
-		else Alice_measurement = 1;
+		int Alice_measurement = rand.nextInt(2);
+		int Alice_bit = rand.nextInt(2);
+		BraKetVector Alice_qbit = new BraKetVector(Alice_bit, Alice_measurement); 
 		
 		//Show Alice data
 		System.out.println("Show Alice data");
 		Alice_qbit.Show();
-		System.out.println(Alice_bit);
 		System.out.println();
 		
 		
-		/*
-		 * data sending
-		 * 
-		 */
+		//Send
+		BraKetVector Data = Send(Alice_qbit, false);
 		
 		
 		//Bob
 		int bob_measurement = rand.nextInt(2);
-		BraKetVector Bob_qbit = Alice_qbit.Measurement(bob_measurement);
+		BraKetVector Bob_qbit = Data.Measurement(bob_measurement);
 		
 		//Show Bob data
 		System.out.println("Show Bob data");
@@ -40,6 +40,40 @@ public class BB84 {
 		
 		System.out.println(Alice_measurement == bob_measurement);
 		
+	}
+	
+	
+	public static BraKetVector Send(BraKetVector qbit, boolean wiretap, boolean info) {
+		if(info) {
+			System.out.println("Send");
+			System.out.println("wiretap: "+wiretap);
+		}
+		
+		if(wiretap == false) return qbit;
+		
+		Random rand = new Random();
+		
+		int Eve_measurement = rand.nextInt(2);
+		BraKetVector tmp = qbit.Measurement(Eve_measurement);
+		
+		if(info) {
+			System.out.println("Eve_measurement: "+Eve_measurement);
+			tmp.Show(true);
+		}
+		
+		System.out.println();
+		
+		return tmp;
+	}
+	
+	public static BraKetVector Send(BraKetVector qbit, boolean wiretap) {
+		if(wiretap == false) return qbit;
+		
+		Random rand = new Random();
+		
+		int Eve_measurement = rand.nextInt(2);
+		
+		return qbit.Measurement(Eve_measurement);
 	}
 
 }
