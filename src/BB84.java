5ed1084728e0d3581716cb9bebce2e8fb4d3c8e1
hika019 <ikazuti2001@gmail.com>
2022-07-13 03:06:40 +0900

    コメントを追加

--- src/BB84.java
+++ src/BB84.java
@@ -1,52 +1,56 @@
 import java.util.Random;
 
 public class BB84 {
-
+    
 	/*
 	 * measurement: 01系, +-系測定のパラメータ 0:01, 1:+-
 	 */
 	
-	public static void main(String[] args) {
+    public static void main(String[] args) {
 		Random rand = new Random();
 		
-		int n = 128;
-		int m = 64;
+		/*
+		 * n: 鍵長
+		 * m: 安全性パラメータ
+		 */
+		int n = (int)Math.pow(50, 4);
+		int m = 100;
 		
 		
+		/*
+		 * aliceBits: Aliceが保存するビット列
+		 * bobBits: Bobが保存するビット列
+		 */
 		int[] aliceBits = new int[n+m];
 		int[] bobBits = new int[n+m];
 		
 		
 		//送信
 		for(int i=0; i<n+m;) {
-			//Alice
+			
+			/* Alice
+			 * aliceMeasurement: 送信する系
+			 * aliceQbit: 送信するqbit
+			 */
 			int aliceMeasurement = rand.nextInt(2);
 			aliceBits[i] = rand.nextInt(2);
 			BraKetVector aliceQbit = new BraKetVector(aliceBits[i], aliceMeasurement); 
 			
-			/*
-			//Show Alice data
-			System.out.println("Show Alice data");
-			aliceQbit.show();
-			System.out.println();
-			*/
+			
+			//NetWork
+			BraKetVector getQbit = network(aliceQbit, true);
 			
 			
-			//Send
-			BraKetVector getData = network(aliceQbit, true);
-			
-			
-			//Bob
+			/* Bob
+			 * bobMeasurement: 測定する系
+			 * bobQbit: 測定後の状態
+			 */
 			int bobMeasurement = rand.nextInt(2);
-			BraKetVector bobQbit = getData.measurement(bobMeasurement);
+			BraKetVector bobQbit = getQbit.measurement(bobMeasurement);
 			bobBits[i] = bobQbit.toBit();
-			/*
-			//Show Bob data
-			System.out.println("Show Bob data");
-			bobQbit.show();
-			System.out.println();
-			*/
 			
+			
+			//系の確認
 			if(aliceMeasurement == bobMeasurement) {
 				i++;
 			}
@@ -54,16 +58,20 @@ public class BB84 {
 		System.out.println("送信終了");
 		System.out.println();
 		
-		//盗聴確認
+		
+		/* パラメータ
+		 * start: 盗聴の確認を行う際の開始地点
+		 * wiretap: 盗聴の有無
+		 */
 		int start = rand.nextInt(n);
 		boolean wiretap = false;
 		
+		//盗聴の確認
 		for(int i=start; i<start+m; i++) {
 			wiretap = wiretap || (aliceBits[i]!=bobBits[i]);
 		}
 		
-		//print
-		
+		//結果の出力
 		System.out.println("wiretap: "+wiretap);
 		System.out.print("Alice:\n");
 		for(int i=0; i<n+m; i++) {
